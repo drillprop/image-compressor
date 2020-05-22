dce8364588570bbe51595b7b76f82d0dd4384657
drillprops <drylbartosz@gmail.com>
2020-05-23 01:44:00 +0200

    create index.html and load it to appWindow

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { app, BrowserWindow } from 'electron';
+import path from 'path';
 
 const initializeAppWindow = () => {
   const appWindow = new BrowserWindow({
     width: 400,
     height: 600,
   });
+  appWindow.loadFile(path.join(__dirname, '../index.html'));
 };
 
 app.on('ready', initializeAppWindow);
