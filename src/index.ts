73d3c9044c5f5805f9bad7c0492d89fd5b443b3e
drillprops <drylbartosz@gmail.com>
2020-05-23 12:05:34 +0200

    setup BrowserWindow config dev environment

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import { app, BrowserWindow } from 'electron';
 import path from 'path';
 
+const isDev = process.env.NODE_ENV === 'development';
+
 const initializeAppWindow = () => {
   const appWindow = new BrowserWindow({
     width: 400,
     height: 600,
+    resizable: isDev ? true : false,
+    x: isDev ? -1000 : undefined, // show app in second screen
+    y: isDev ? 0 : undefined,
   });
+
   appWindow.loadFile(path.join(__dirname, '../index.html'));
 };
 
