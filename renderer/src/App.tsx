825e7b242a568fca1b21e22efbefa7c0d831da7d
drillprop <drylbartosz@gmail.com>
2020-07-30 22:12:05 +0200

    create useRangeInput

--- renderer/src/App.tsx
+++ renderer/src/App.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import { IpcRenderer } from 'electron';
+import React, { useState } from 'react';
+import { IpcRenderer, AllElectron } from 'electron';
 import RangeInput from './components/RangeInput';
 import FileInput from './components/FileInput';
-const electron = window.require('electron'); // require electron like this in all the files. Don't Use import from 'electron' syntax for importing IpcRender from electron.
+import useRangeInput from './hooks/useRangeInput';
+
+const electron: AllElectron = window.require('electron'); // require electron like this in all the files. Don't Use import from 'electron' syntax for importing IpcRender from electron.
 
 let ipcRenderer: IpcRenderer = electron.ipcRenderer;
 
@@ -10,17 +12,36 @@ ipcRenderer.on('response', (_, args: any[]) => {
   console.log(args);
 });
 
-function App() {
+const App = () => {
+  const { handleRanges, ranges } = useRangeInput({
+    width: 0,
+    height: 0,
+    quality: 0,
+  });
+
   return (
     <div className='text-center w-3/4 mx-auto'>
       <h1 className='mt-4 font-bold uppercase text-2xl'>image compressor</h1>
       <form className='flex flex-col mt-10 items-center'>
         <FileInput />
         <div className='mt-6 flex justify-between w-full'>
-          <RangeInput inputName='width' />
-          <RangeInput inputName='height' />
+          <RangeInput
+            onChange={handleRanges}
+            value={ranges.width}
+            inputName='width'
+          />
+          <RangeInput
+            onChange={handleRanges}
+            value={ranges.height}
+            inputName='height'
+          />
         </div>
-        <RangeInput wFull inputName='quality' />
+        <RangeInput
+          onChange={handleRanges}
+          value={ranges.quality}
+          wFull
+          inputName='quality'
+        />
         <button
           className='mt-10 w-full py-2 cursor-not-allowed bg-gray-500 text-white font-medium'
           type='submit'
@@ -31,6 +52,6 @@ function App() {
       </form>
     </div>
   );
-}
+};
 
 export default App;
--- renderer/src/components/RangeInput.tsx
+++ renderer/src/components/RangeInput.tsx
@@ -3,13 +3,17 @@ import React from 'react';
 type Props = {
   inputName: string;
   wFull?: boolean;
+  value: number;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-const RangeInput: React.FC<Props> = ({ inputName, wFull }) => {
+const RangeInput: React.FC<Props> = ({ inputName, wFull, onChange, value }) => {
   return (
     <label className={`mt-6 ${wFull && 'w-full'}`}>
       <span className='block capitalize'>{inputName}</span>
       <input
+        onChange={onChange}
+        value={value}
         className='bg-gray-500 block w-full mt-3'
         min='0'
         max='100'
