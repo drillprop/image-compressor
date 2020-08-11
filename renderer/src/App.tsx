9475c8d019e80a3544f02aee79ca564d564f9a89
drillprop <drylbartosz@gmail.com>
2020-08-11 18:50:51 +0200

    add green background

--- renderer/src/App.tsx
+++ renderer/src/App.tsx
@@ -1,6 +1,5 @@
 import { AllElectron, IpcRenderer } from 'electron';
-import React, { useState } from 'react';
-import useRangeInput from './hooks/useRangeInput';
+import React from 'react';
 import UploadForm from './components/UploadForm/UploadForm';
 
 const electron: AllElectron = window.require('electron'); // require electron like this in all the files. Don't Use import from 'electron' syntax for importing IpcRender from electron.
@@ -13,9 +12,11 @@ ipcRenderer.on('response', (_, args: any[]) => {
 
 const App = () => {
   return (
-    <div className='text-center w-3/4 mx-auto'>
-      <h1 className='mt-4 font-bold uppercase text-2xl'>image compressor</h1>
-      <UploadForm />
+    <div className='w-full bg-green-100 h-screen py-6'>
+      <div className='text-center w-3/4 mx-auto'>
+        <h1 className='font-bold uppercase text-2xl'>image compressor</h1>
+        <UploadForm />
+      </div>
     </div>
   );
 };
