3f4756e718d3670a795d508b5aaca1a78ca5dbea
drillprop <drylbartosz@gmail.com>
2020-08-11 18:51:52 +0200

    update UploadForm styles

--- renderer/src/components/UploadForm/UploadForm.tsx
+++ renderer/src/components/UploadForm/UploadForm.tsx
@@ -4,8 +4,8 @@ import { ReactComponent as UploadIcon } from '../../assets/upload-icon.svg';
 
 const UploadForm = () => {
   return (
-    <form className='flex flex-col mt-12 items-center'>
-      <h2 className='text-xl font-bold'>UPLOAD AN IMAGE</h2>
+    <form className=' mt-5 flex flex-col items-center'>
+      <h2 className='text-lg font-bold'>UPLOAD AN IMAGE</h2>
       <label className=' flex items-center justify-center rounded mt-3 cursor-pointer w-10/12 py-4 bg-green-700 hover:bg-green-800 text-white font-medium'>
         <div className='mr-2'>
           <ImageIcon />
@@ -13,7 +13,7 @@ const UploadForm = () => {
         <span> Choose an Image</span>
         <input className='hidden' type='file' name='file' id='file' />
       </label>
-      <h2 className='mt-8 text-xl font-bold'>OR</h2>
+      <h2 className='mt-5 text-xl font-bold'>OR</h2>
       <div
         className='rounded mt-3 w-full h-48 flex items-center justify-center
          bg-green-600 text-white font-medium'
