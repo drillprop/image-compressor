c1f85cdb74e731c26dd726adf2eb50f81dc0de3b
drillprop <drylbartosz@gmail.com>
2020-08-12 20:46:30 +0200

    set steps for components

--- renderer/src/App.tsx
+++ renderer/src/App.tsx
@@ -1,6 +1,9 @@
 import { AllElectron, IpcRenderer } from 'electron';
 import React from 'react';
+import RangesForm from './components/RangesForm/RangesForm';
+import SuccessView from './components/SuccessView/SuccessView';
 import UploadForm from './components/UploadForm/UploadForm';
+import { useGlobalState } from './context/GlobalContext';
 
 const electron: AllElectron = window.require('electron'); // require electron like this in all the files. Don't Use import from 'electron' syntax for importing IpcRender from electron.
 
@@ -11,11 +14,14 @@ ipcRenderer.on('response', (_, args: any[]) => {
 });
 
 const App = () => {
+  const state = useGlobalState();
   return (
     <div className='w-full bg-green-100 h-screen py-6'>
       <div className='text-center w-3/4 mx-auto'>
         <h1 className='font-bold uppercase text-2xl'>image compressor</h1>
-        <UploadForm />
+        {state.step === 1 && <UploadForm />}
+        {state.step === 2 && <RangesForm />}
+        {state.step === 3 && <SuccessView />}
       </div>
     </div>
   );
