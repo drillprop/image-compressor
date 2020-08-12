b526382b5d058001571b793d53251a3eeb4d0726
drillprop <drylbartosz@gmail.com>
2020-08-12 11:18:51 +0200

    rename main.css to tailwind.output.css and add to .gitignore

--- renderer/src/index.tsx
+++ renderer/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import './assets/main.css';
+import './assets/tailwind.output.css';
 import './assets/inputs.css';
 
 declare global {
