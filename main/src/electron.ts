c7cca0a2f066eb53c7e6a4ea7ada5f937ea82384
drillprop <drylbartosz@gmail.com>
2020-08-13 16:20:42 +0200

    create change:directory event listener

--- main/src/electron.ts
+++ main/src/electron.ts
@@ -4,10 +4,14 @@ import {
   Menu,
   MenuItem,
   MenuItemConstructorOptions,
+  ipcMain,
+  dialog,
 } from 'electron';
 import * as path from 'path';
 
 const isDev = process.env.NODE_ENV === 'development';
+
+let appWindow: BrowserWindow;
 
 const devMenu: (MenuItemConstructorOptions | MenuItem)[] = [
   {
@@ -29,7 +33,7 @@ const menu: (MenuItemConstructorOptions | MenuItem)[] = [
 ];
 
 const initializeAppWindow = () => {
-  const appWindow = new BrowserWindow({
+  appWindow = new BrowserWindow({
     width: 400,
     height: 600,
     resizable: isDev ? true : false,
@@ -56,4 +60,15 @@ app.on('ready', () => {
 
   const mainMenu = Menu.buildFromTemplate(menu);
   Menu.setApplicationMenu(mainMenu);
+});
+
+ipcMain.on('change:directory', async (e, options) => {
+  if (!appWindow) return;
+  const directory = await dialog.showOpenDialog(appWindow, {
+    properties: ['openDirectory'],
+  });
+  if (!directory.canceled) {
+    const [outputFolder] = directory.filePaths;
+    appWindow.webContents.send('change:directory', outputFolder);
+  }
 });
